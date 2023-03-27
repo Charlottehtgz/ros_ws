9d244d3bf1ddb2399018e77d087b93afb610f8c2
Charlottehtgz <Charlottehtgz@gmail.com>
2023-03-27 12:06:25 +0100

    initial commit

---
+++ build/ros-vosk/catkin_generated/ros_vosk-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(ros_vosk_MESSAGE_FILES "/root/ros_ws/src/ros-vosk/msg/speech_recognition.msg")
+set(ros_vosk_SERVICE_FILES "")
---
+++ build/test_exec/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/ros_ws/src/test_exec
+# Build directory: /root/ros_ws/build/test_exec
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
